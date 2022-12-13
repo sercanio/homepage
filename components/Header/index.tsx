af9731bc0bb7ca9f1d9bfa29c7bdc63d8f45f582
Sercan ATEŞ <sercanates91@gmail.com>
2022-12-13 22:27:43 +0300

    Create About page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import Toggle from '../Button/Toggle'
+import { Toggle } from '../Button'
 import Link from 'next/link'
 import Image from 'next/image'
+
 const Header = () => {
   // Redux dispatch
   return (
-    <header className="bg-skin-base py-3 px-6">
-      <nav className="flex justify-between py-1 px-2 ">
-        <div className="flex justify-between items-center">
+    <header className="bg-skin-base py-3 px-4">
+      <nav className="flex justify-between">
+        <div className="flex items-center">
           <div className="flex items-center">
-            <Link href="/">
+            {/* <Link href="/">
               <Image
                 src="/images/logo.svg"
                 alt="Logo"
@@ -17,9 +18,9 @@ const Header = () => {
                 width={64}
                 height={64}
               />
-            </Link>
+            </Link> */}
             <h1 className="text-2xl font-bold text-skin-base">
-              <Link href="/">Sercan</Link>
+              <Link href="/">sercan.io</Link>
             </h1>
           </div>
         </div>
@@ -34,7 +35,7 @@ const Header = () => {
             <Link href="/contact">Portfolio</Link>
           </li>
           <li>
-            <Link href="/Linkbout">About</Link>
+            <Link href="/about">About</Link>
           </li>
         </ul>
       </nav>
---
+++ pages/about/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const index = () => {
+  return <div className="text-skin-base">About</div>
+}
+
+export default index
