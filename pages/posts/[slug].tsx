7e9c01a2fc5d9215ca95c8c809919d705d338c91
Sercan Ateş <sercanates91@gmail.com>
2022-12-24 21:50:55 +0300

    Remove sections

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -38,7 +38,7 @@ export default function PostPage({ post }: { post: MDXPost }) {
         <title>{post.meta.title}</title>
         <meta name="description" content={post.meta.title} />
       </Head>
-      <article className="relative flex gap-20 p-3 max-w-5xl h-full mx-auto z-0">
+      <article className="relative p-3 max-w-5xl h-full mx-auto z-0">
         <div className="relative z-10 flex-5">
           <h2 className="text-3xl font-bold mb-4">{post.meta.title}</h2>
           {post.meta.tags && (
@@ -99,9 +99,6 @@ export default function PostPage({ post }: { post: MDXPost }) {
             </TelegramShareButton>
           </div>
           <hr className="pb-4 border-t-1 mt-3 -mb-2" />
-        </div>
-        <div className="hidden sm:block w-64 flex-1">
-          <h2>Sections</h2>
         </div>
       </article>
     </>
