559d94d9b61d213af901c07a0999b3b88bf15813
Sercan Ateş <sercanates91@gmail.com>
2022-12-24 10:58:07 +0300

    Fix layouting

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 // Styorybook styling
 const Header: React.FC = () => {
   return (
-    <header className="bg-skin-base dark:bg-black color-transition py-4 px-4 relative">
-      <nav className="flex justify-between items-baseline relative z-10 mb-1 max-w-5xl lg:px-4 mx-auto">
+    <header className="bg-skin-base dark:bg-black color-transition py-4 px-6 relative">
+      <nav className="flex justify-between items-baseline relative z-10 mb-1 max-w-5xl lg:px-6 mx-auto">
         <div className="flex items-center">
           <div className="flex items-center">
             <h1 className="text-2xl sm:text-3xl font-bold   -ml-2">
