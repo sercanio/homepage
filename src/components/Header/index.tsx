b2b66670240b74e48bb633adbade8ac78beeccea
Sercan Ateş <sercanates91@gmail.com>
2022-12-24 11:15:01 +0300

    Fix spacing issues

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,26 @@ import Link from 'next/link'
 // Styorybook styling
 const Header: React.FC = () => {
   return (
-    <header className="bg-skin-base dark:bg-black color-transition py-4 px-6 relative">
-      <nav className="flex justify-between items-baseline relative z-10 mb-1 max-w-5xl lg:px-6 mx-auto">
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <h1 className="text-2xl sm:text-3xl font-bold   -ml-2">
-              <Link href="/" className="flex items-center">
-                <span className=" text-skin-primary">
-                  <span className=" text-skin-primary">sercan</span>
-                  @web{':'}
-                  <span className=" text-skin-tilde">~</span>
-                  {'$'}
-                  &nbsp;
-                </span>
-                <span className="animate-cursorBlink mb-1  text-skin-caret ">
-                  |
-                </span>
-                <h1 className="sr-only">
-                  {/* eslint-disable-next-line react/no-unescaped-entities*/}
-                  Sercan Ateş's personal web logs
-                </h1>
-              </Link>
+    <header className="bg-skin-base dark:bg-black color-transition pb-4 pt-5">
+      <nav className="z-10 mb-1 max-w-5xl mx-auto px-3">
+        <h1 className="text-2xl sm:text-3xl font-bold">
+          <Link href="/" className="flex items-center">
+            <span className=" text-skin-primary">
+              <span className=" text-skin-primary">sercan</span>
+              @web{':'}
+              <span className=" text-skin-tilde">~</span>
+              {'$'}
+              &nbsp;
+            </span>
+            <span className="animate-cursorBlink mb-1  text-skin-caret ">
+              |
+            </span>
+            <h1 className="sr-only">
+              {/* eslint-disable-next-line react/no-unescaped-entities*/}
+              Sercan Ateş's personal web logs.
             </h1>
-          </div>
-        </div>
+          </Link>
+        </h1>
       </nav>
     </header>
   )
