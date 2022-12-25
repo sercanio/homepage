c6e4aaf7011b0fc55db14dab92e83d81c28c23d7
Sercan Ateş <sercanates91@gmail.com>
2022-12-25 10:36:56 +0300

    Delete custom 404 page

--- pages/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function p404() {
-  return (
-    <div className="h-full flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">404</h1>
-      <p className="mt-4 text-xl">I have not built this page yet...</p>
-    </div>
-  )
-}
