989fe949737c9836d7cc06609f8a3740a9a5d79f
Sercan Ateş <sercanates91@gmail.com>
2022-12-25 22:15:55 +0300

    Change left margin of Related section items

--- src/components/CreateRelatedArticles/index.tsx
+++ src/components/CreateRelatedArticles/index.tsx
@@ -7,7 +7,7 @@ const CreateRelatedArticles = ({ post }: { post: MDXPost }) => {
     <section className="px-2 xl:px-4 pt-4 pb-8 xl:block h-max w-full max-w-5xl  mx-auto">
       {post.meta.related.length !== 0 && (
         <>
-          <div className=" p-2">
+          <div className="p-2">
             <strong className="uppercase font-bold px-2">Related</strong>
           </div>
           <hr className="border-t-1 xl:border-t-2 dark:border-skin-dark mb-2" />
@@ -23,7 +23,7 @@ const CreateRelatedArticles = ({ post }: { post: MDXPost }) => {
               <Link
                 href={`/posts${related.url}`}
                 key={related.title}
-                className="ml-4"
+                className="ml-2"
               >
                 {related.title}
               </Link>
