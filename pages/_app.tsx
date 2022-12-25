538b8e9fb52b2855b8656786f6e45436eeadbc03
Sercan Ateş <sercanates91@gmail.com>
2022-12-25 23:51:06 +0300

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import { Roboto } from '@next/font/google'
 import Layout from './layout'
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout className={roboto.className}>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
