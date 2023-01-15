a9b604558f04b6507d87ee914f24341f17fc3e22
Sercan Ateş <sercanates91@gmail.com>
2023-01-15 14:13:20 +0300

    Reduce contrast in dark mode by changing background color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className="bg-skin-base dark:bg-black color-transition">
+    <header className="bg-skin-base dark:bg-skin-dark color-transition">
       <nav className="z-10 mb-1 max-w-5xl xl:max-w-[1310px] mx-auto px-3">
         <h1 className="text-2xl sm:text-3xl font-bold">
           <Link href="/" className="flex items-center">
