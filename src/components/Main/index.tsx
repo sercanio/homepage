a2d58e933a93aeecd3f02eee983fa86933a89bde
Sercan Ateş <sercanates91@gmail.com>
2023-04-12 14:49:04 +0300

    Fix for blog page position

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,10 @@
 import { MainTypes } from '@/types'
 
 const Main = ({ children, className }: MainTypes) => {
-  return <main className={`${className}`}>{children}</main>
+  return (
+    <main className={`${className} max-w-5xl mx-auto mt-20 px-1`}>
+      {children}
+    </main>
+  )
 }
 export default Main
