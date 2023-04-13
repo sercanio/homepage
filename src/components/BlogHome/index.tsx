339215c04e17899ff1b10c0e8aee38c60b2aea63
Sercan Ateş <sercanates91@gmail.com>
2023-04-13 18:13:03 +0300

    Improve responsiveness

--- src/components/BlogHome/index.tsx
+++ src/components/BlogHome/index.tsx
@@ -11,7 +11,7 @@ const BlogHome = ({
   filteredPosts: PostMeta[]
 }) => {
   return (
-    <div className="flex flex-col md:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-screen scroll-smooth justify-between w-full max-w-5xl mt-20 px-1">
+    <div className="flex flex-col md:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-screen scroll-smooth justify-between w-full max-w-5xl mt-16 md:mt-20 md:mb-6 px-1">
       <CreateArticleList posts={filteredPosts} />
       <aside
         className="sticky top-16 flex flex-col gap-1 h-max max-w-[280px]"
