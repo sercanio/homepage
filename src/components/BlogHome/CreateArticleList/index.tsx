290c13e00812b9d4dc0cfdbfbcd30634feeadc53
Sercan Ateş <sercanates91@gmail.com>
2023-10-10 22:15:20 +0300

    Change post list gap sizes

--- src/components/BlogHome/CreateArticleList/index.tsx
+++ src/components/BlogHome/CreateArticleList/index.tsx
@@ -8,13 +8,13 @@ const CreateArticleList = ({ posts }: { posts: PostMeta[] }) => {
 
   return (
     <section>
-      <ul className="relative z-0 flex flex-col gap-6 w-full xl:mx-0 px-[2px] xl:pr-1">
+      <ul className="relative z-0 flex flex-col gap-8 w-full xl:mx-0 px-[2px] xl:pr-1">
         {posts.map(post => (
           <li
             key={post.slug}
             className="relative flex flex-col px-3 bg-skin-baseCard hover:bg-skin-baseCardHover dark:bg-skin-darkCard dark:hover:bg-skin-darkCardHover [&>div>h2>a] dark:[&>div>h2>a] rounded-md"
           >
-            <div className="z-10 flex flex-col gap-1">
+            <div className="z-10 flex flex-col gap-0">
               <h2 className="font-bold text-2xl text-gradient-hero">
                 <Link href={`/posts/${post.slug}`}>{post.title}</Link>
               </h2>
