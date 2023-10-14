9cdf2a79ec1383b22b38cb8b394cb2aab740be69
Sercan Ateş <sercanates91@gmail.com>
2023-10-14 09:43:59 +0300

    Change site font to Rubik

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,11 +2,16 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Main from '@/components/Main'
 import { IChildren } from '@/types'
+import { Rubik } from '@next/font/google'
 
+const inter = Rubik({
+  weight: '400',
+  subsets: ['latin']
+})
 const Layout = ({ children }: IChildren) => {
   return (
     <div
-      className={`relative flex flex-col justify-center bg-skin-base dark:bg-skin-dark text-skin-base dark:text-skin-dark color-transition pt-1`}
+      className={`${inter.className} relative flex flex-col justify-center bg-skin-base dark:bg-skin-dark text-skin-base dark:text-skin-dark color-transition pt-1`}
     >
       <Header />
       <Main>{children}</Main>
