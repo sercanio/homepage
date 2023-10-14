1ac604c384e3eeaa43428799e4b411b6af6c81a3
Sercan Ateş <sercanates91@gmail.com>
2023-10-14 10:03:42 +0300

    Change positionings

--- src/components/BlogHome/index.tsx
+++ src/components/BlogHome/index.tsx
@@ -12,10 +12,10 @@ const BlogHome = ({
   filteredPosts: PostMeta[]
 }) => {
   return (
-    <div className="flex flex-col xl:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-screen scroll-smooth justify-between w-full max-w-5xl mt-16 md:mt-20 md:mb-6 px-1">
+    <div className="flex flex-col xl:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-screen scroll-smooth justify-between mt-16 md:mt-20 md:mb-6 px-1 max-w-[750px] xl:max-w-5xl">
       <CreateArticleList posts={filteredPosts} />
       <aside
-        className="sticky top-16 flex flex-col gap-1 h-max max-w-[280px]"
+        className="sticky top-16 flex flex-col sm:flex-row xl:flex-col gap-1 h-max"
         style={{ contentVisibility: 'auto' }}
       >
         <CreateAllTags posts={posts} />
