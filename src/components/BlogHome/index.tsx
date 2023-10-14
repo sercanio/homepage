c83822b3b9cb4cb2f15621fa175139ba50aa9a98
Sercan Ateş <sercanates91@gmail.com>
2023-10-14 10:17:27 +0300

    Fix layout overflow in landscape mode

--- src/components/BlogHome/index.tsx
+++ src/components/BlogHome/index.tsx
@@ -12,10 +12,10 @@ const BlogHome = ({
   filteredPosts: PostMeta[]
 }) => {
   return (
-    <div className="flex flex-col xl:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-screen scroll-smooth justify-between mt-16 md:mt-20 md:mb-6 px-1 max-w-[750px] xl:max-w-5xl">
+    <div className="flex flex-col xl:flex-row gap-1 xl:gap-4 -ml-[10px] xl:-ml-5 h-full scroll-smooth justify-between mt-16 md:mt-20 md:mb-6 px-1 max-w-[750px] xl:max-w-5xl">
       <CreateArticleList posts={filteredPosts} />
       <aside
-        className="sticky top-16 flex flex-col sm:flex-row xl:flex-col gap-1 h-max"
+        className="sticky top-16 flex flex-col sm:flex-row xl:flex-col gap-1 h-max mt-20 xl:mt-0"
         style={{ contentVisibility: 'auto' }}
       >
         <CreateAllTags posts={posts} />
