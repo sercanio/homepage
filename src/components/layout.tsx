299b9a599c33bee759fa9b1de2ae990d65c1e521
Sercan Ateş <104576153+sercanio@users.noreply.github.com>
2023-10-16 23:06:31 +0300

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ const inter = Rubik({
 const Layout = ({ children }: IChildren) => {
   return (
     <div
-      className={`${inter.className} relative flex flex-col justify-center bg-skin-base dark:bg-skin-dark text-skin-base dark:text-skin-dark color-transition pt-1 dark:bg-gradient-to-tr dark:from-[#090c18] dark:to-[#301818]`}
+      className={`${inter.className} relative flex flex-col justify-center bg-skin-base dark:bg-skin-dark text-skin-base dark:text-skin-dark color-transition pt-1`}
     >
       <Header />
       <Main>{children}</Main>
